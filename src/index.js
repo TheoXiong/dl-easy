199ffd4012bde1f229d4550ab18165d3b6a70e1d
zhi.xiong <zhi.xiong@xtalpi.com>
2019-01-03 17:11:31 +0800

    add capture destroyed status

--- src/index.js
+++ src/index.js
@@ -5,19 +5,34 @@ const download = (win, url = '', options = {}) => {
     }
     
     const listener = (event, item, webContents) => {
+      let isStart = false
+      let isOver = false
+      setTimeout(() => {
+        if (item && typeof item.isDestroyed === 'function') {
+          // download item has been destroyed, but the event has not been captured
+          if (isStart && !isOver && item.isDestroyed()) {
+            if (typeof options.onCancel === 'function') {
+              options.onCancel(item)
+            }
+          } 
+        }
+      }, 2000)
+
       if (typeof options.onStart === 'function') {
+        isStart = true
         options.onStart(item)
       }
 
       item.once('done', (event, state) => {
+        isOver = true
         if (state === 'completed') {
-          resolve(item)
+          return resolve(item)
         } else if (state === 'cancelled') {
           if (typeof options.onCancel === 'function') {
             options.onCancel(item)
           }
         } else if (state === 'interrupted') {
-          reject(new Error('download interrupted'))
+          return reject(new Error('download interrupted'))
         }
       })
     }
